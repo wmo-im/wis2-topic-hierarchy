23ad2ef786bef6bd852a7300eb0c29ec8f9d75d6
Tom Kralidis <tomkralidis@gmail.com>
2024-03-12 15:13:04 -0400

    updates

--- scripts/codelists2ttl.py
+++ scripts/codelists2ttl.py
@@ -122,19 +122,19 @@ def gen_skos_concept(name: str, description: str, source: str = None) -> str:
 
 
 def read_subdomain_index(index_file):
-    with open(index_file) as fh_index:
+    with index_file.open() as fh_index:
         reader_index = csv.DictReader(fh_index)
         index_file_path = os.path.abspath(index_file)
         sub_dir_part = index_file_path.split('/earth-system-discipline/')[1]
         sub_dir = sub_dir_part.replace('/index.csv', '')
         for row3 in reader_index:
             if sub_dir != '':
-                concept_ttl_dir = f'{register_ttl_dir}/{sub_dir}'
-                concept_ttl_file = f"{concept_ttl_dir}/{row3['Name']}.ttl"
+                concept_ttl_dir = register_ttl_dir / sub_dir
+                concept_ttl_file = concept_ttl_dir / f"{row3['Name']}.ttl"
             else:
-                concept_ttl_file = f"{register_ttl_dir}/{row3['Name']}.ttl"
+                concept_ttl_file = register_ttl_dir / f"{row3['Name']}.ttl"
             print(f'Generating {concept_ttl_file}')
-            with open(concept_ttl_file, 'w') as fh2_index:
+            with concept_ttl_file.open('w') as fh2_index:
                 ttl = gen_skos_concept(row3['Name'],
                                        row3['Description'])
                 fh2_index.write(ttl)
@@ -161,6 +161,14 @@ ttl_files_path.mkdir(parents=True)
 
 root_table = ROOTPATH / 'topic-hierarchy.csv'
 
+
+root_ttl = ROOTPATH / 'wis' / 'topic-hierarchy.ttl'
+with root_ttl.open('w') as fh:
+    ttl = gen_skos_subregister('topic-hierarchy',
+                               'WIS2 Topic Hierarchy')
+    fh.write(ttl)
+
+
 with root_table.open() as fh:
     subregisters = []
     reader = csv.DictReader(fh)
@@ -169,11 +177,11 @@ with root_table.open() as fh:
         subregister_url = 'http://codes.wmo.int/wis/topic-hierarchy'
         subregisters.append(f'<{subregister_url}>')
         register_ttl_dir = ttl_files_path
-        register_ttl_file = f"{register_ttl_dir}/{row['Name']}.ttl"
+        register_ttl_file = register_ttl_dir / f"{row['Name']}.ttl"
         print(f'Generating {register_ttl_file}')
 
-        if 'earth-system-discipline' not in str(row['Name']):
-            with open(register_ttl_file, 'w') as fh2:
+        if row['Name'] != 'earth-system-discipline':
+            with register_ttl_file.open('w') as fh2:
                 ttl = gen_skos_subregister(row['Name'],
                                            row['Description'])
                 fh2.write(ttl)
@@ -192,14 +200,22 @@ with root_table.open() as fh:
                         ttl = gen_skos_concept(row2['Name'],
                                                row2['Description'])
                         fh4.write(ttl)
+        else:
+            with register_ttl_file.open('w') as fh2:
+                ttl = gen_skos_subregister(row['Name'],
+                                           row['Description'])
+                fh2.write(ttl)
 
-print('Level 1-7 READY')
+print('Level 1-7 completed')
+
+print('Generating Level 8-')
+
 root_table_sub = ROOTPATH / 'topic-hierarchy/earth-system-discipline/index.csv'
 ttl_root_dir = ROOTPATH / 'wis/topic-hierarchy/earth-system-discipline'
 if not os.path.exists(ttl_root_dir):
     ttl_root_dir.mkdir()
 
-with open(root_table_sub) as fh5:
+with root_table_sub.open() as fh5:
     subregisters = []
     reader = csv.DictReader(fh5)
     subreg_base = 'http://codes.wmo.int/wis/topic-hierarchy'
@@ -210,23 +226,22 @@ with open(root_table_sub) as fh5:
         subregister_url = f"{subreg_baseurl}/{row['Name']}"
         subregisters.append(f'<{subregister_url}>')
         register_ttl_dir = ttl_files_path
-        register_ttl_file = f"{register_ttl_dir}/{row['Name']}.ttl"
+        register_ttl_file = register_ttl_dir / f"{row['Name']}.ttl"
         print(f'Generating {register_ttl_file}')
         if not os.path.exists(register_ttl_dir):
             register_ttl_dir.mkdir()
 
-        with open(register_ttl_file, 'w') as fh6:
+        with register_ttl_file.open('w') as fh6:
             ttl = gen_skos_subregister(row['Name'],
                                        row['Description'])
             fh6.write(ttl)
 
         concept_dir_part = 'topic-hierarchy/earth-system-discipline'
         concept_csv_dir = ROOTPATH / f"{concept_dir_part}/{row['Name']}"
-        concept_csv_file = f'{concept_csv_dir}/index.csv'
-        concept_ttl_path = Path(f"{ttl_files_path}/{row['Name']}")
+        concept_csv_file = concept_csv_dir / 'index.csv'
+        concept_ttl_path = ttl_files_path / row['Name']
         if not os.path.exists(concept_ttl_path):
             concept_ttl_path.mkdir()
-        concept_ttl_file = f"{ttl_files_path}/{row['Name']}.ttl"
 
         read_subdomain_index(concept_csv_file)
 
